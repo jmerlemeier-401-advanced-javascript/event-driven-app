45bee3864dcbc0fa0a3912b1af9b07f2acec0642
Julie Erlemeier <jmerlemeier@gmail.com>
2019-11-05 15:25:28 -0800

    Added started code and ready to refactor.

--- modular/app.js
+++ modular/app.js
@@ -1,15 +1,37 @@
 'use strict';
 
-const events = require('./events');
-// const EventEmitter = new Events();
+const fs = require('fs');
 
-//loggin activity to the console
-const logger = require('./logger');
+/**
+ * 
+ * @param {*} file 
+ */
+const alterFile = (file) => {
+  fs.readFile( file, (err, data) => {//util.promisify
+    if(err) { throw err; }
+    let text = data.toString().toUpperCase(); //could be broken down.
+    fs.writeFile( file, Buffer.from(text), (err, data) => {
+      if(err) { throw err; }
+      console.log(`${file} saved`);
+      //eventemitter.emit()
+    });
+  });
+};
 
-//handle our data cache
+let file = process.argv.slice(2).shift();
+alterFile(file);
 
+//================ from lecture ================
+// const events = require('./events');
+// // const EventEmitter = new Events();
 
-//'save' is the flag and then the string is the payload response.
-//Great for automated systems.
-events.emit('save', 'Jacob is super rad');
+// //loggin activity to the console
+// require('./logger');
 
+// //handle our data cache
+// require('./cache');
+
+// //'save' is the flag and then the string is the payload response.
+// //Great for automated systems.
+// events.emit('save', {id: 01, content: 'Jacob is super rad'});
+// events.emit('delete', {id: 01, content: 'Jacob is super rad'});
--- modular/logger.js
+++ modular/logger.js
@@ -3,6 +3,9 @@
 const events = require('./events');
 
 events.on('save', payload => log('save', payload));
+events.on('delete', payload => log('delete', payload));
+events.on('update', payload => log('update', payload));
+
 
 //event is 'save' abd the payload is whatever is passed in on the emission
 function log(event, payload) {
